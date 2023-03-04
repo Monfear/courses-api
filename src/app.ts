f9d018fb03938928a69b148e998496f62a2d0140
Patryk Wnukowski <wnuq@ymail.com>
2023-03-04 09:40:00 +0100

    create logger instance and add type

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,25 @@
 import express, { Express } from "express";
 import router from './routers/router';
+import { LEVELS } from "./types/Levels.enum";
 import { configDotenv } from "./utils/dotenv";
-import { logger } from "./utils/logger";
-// import { logger } from "./utils/logger";
+import { Logger } from "./utils/logger";
 
 class App {
     private port: number = 3000;
     private hostname: string = 'localhost';
     private app: Express = express();
 
+    private logger: Logger;
+
     constructor() {
         configDotenv();
+        this.logger = new Logger(LEVELS.INFO);
     };
 
     public init(): void {
         try {
             this.app.listen(this.port, this.hostname, (): void => {
-                logger.info(`app is listening on ${this.hostname} at port ${this.port}`);
+                this.logger._logger.info(`app is listening on ${this.hostname} at port ${this.port}`);
             });
 
             this.setupRouters();
@@ -30,7 +33,7 @@ class App {
     private setupRouters(): void {
         this.app.use('/', router);
     };
-}
+} 
 
 (function runApp(): void {
     new App().init();
