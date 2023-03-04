76aa85e5d9a48925558ee8b531411386eb901759
Patryk Wnukowski <wnuq@ymail.com>
2023-03-04 09:41:39 +0100

    create enum logger levels

--- src/types/Levels.enum.ts
+++ src/types/Levels.enum.ts
@@ -1,3 +1,9 @@
-export enum LEVELS {
-    TEST = 'TEST',
-}
+export enum LEVELS  {
+    ERROR = 'error',
+    warn = 'warn',
+    INFO = 'info',
+    HTTP = 'http',
+    VERBOSE = 'verbose',
+    DEBUG = 'debug',
+    SILLY = 'silly',
+};
