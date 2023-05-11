fba6b561ffc437dabdee6266e7f50ee7cfab5793
Patryk Wnukowski <wnuq@ymail.com>
2023-05-11 07:18:05 +0200

    create user post route and controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response, RequestHandler } from "express";
 import { User } from "../models/user.model";
+import { IUser } from "../types/User.interface";
+import { hashPassword } from "../functions/hashPassword";
 
+
+// @ GET
 export const showAllUsers: RequestHandler = async (req: Request, res: Response) => {
     try {
         const users: User[] = await User.find();
@@ -26,4 +30,45 @@ export const showAllUsers: RequestHandler = async (req: Request, res: Response) 
             });
         };
     }
+};
+
+// * POST
+export const createUser: RequestHandler = async (req: Request, res: Response) => {
+    try {
+        const userData: IUser = req.body;
+        const { name, email, plainTextPassword, passwordSalt, isAdmin } = userData;
+
+        let passwordHash: string | undefined = undefined;
+
+        passwordHash = await hashPassword(plainTextPassword, passwordSalt);
+
+        const user: User = User.create({
+            name,
+            email,
+            passwordSalt,
+            passwordHash,
+            isAdmin
+        });
+
+        await user.save();
+
+        console.log({
+            success: true,
+            msg: 'User created.',
+            data: user
+        })
+
+        return res.status(201).json({
+            success: true,
+            msg: 'User created.',
+            data: passwordHash
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(500).json({
+                success: false,
+                errMsg: error.message
+            });
+        };
+    };
 };
--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { showAllUsers } from "../controllers/users.controller";
+import { createUser, showAllUsers } from "../controllers/users.controller";
 
 const usersRouter: Router = Router();
 
 usersRouter
     .get('/', showAllUsers)
+    .post('/', createUser)
 
 export default usersRouter;
