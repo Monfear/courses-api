bfb28d7a1bba2fd2e44c81b5784cc12be64ecb35
Patryk Wnukowski <wnuq@ymail.com>
2023-05-26 07:27:27 +0200

    unlock validators

--- src/app.ts
+++ src/app.ts
@@ -68,9 +68,9 @@ class App {
         }));
         this.app.use(showRequestInfo);
 
-        // this.app.use('/api/courses', validateUser);
-        // this.app.use('/api/lessons', validateUser);
-        // this.app.use('/api/users', validateUser);
+        this.app.use('/api/courses', validateUser);
+        this.app.use('/api/lessons', validateUser);
+        this.app.use('/api/users', validateUser);
     };
 };
 
